5ccc453fc1f65d8e0c63e70434888f68cef5f340
Yuri Kl <yurikl@gmail.com>
2013-10-12 16:00:54 +0300

    Added one more initial project in Hebrew so I can see how the RTL looks, also fixed definition to remove entries from the project table on down, not sure why it is required when we drop the table is 1.sql, but it is. Maybe to support updating 2.sql without running 1.sql

--- conf/evolutions/default/2.sql
+++ conf/evolutions/default/2.sql
@@ -5,7 +5,9 @@
 
 insert into project (id,name, description, votes) values (  9,'Ninja turtles tribute site','What it sounds like', 0 );
 insert into project (id,name, description, votes) values (  10,'Coffee bags startup','Why should those tea bastards  enjoy progress alone ? ', 0 );
-insert into project (id,name, description, votes) values (  11,'Haskell support event','More people should know about Haskell', 0 );
+insert into project (id,name, description, votes) values (  11,'Haskell support event','More people should know about Haskell', 2 );
+insert into project (id,name, description, votes) values (  12,'הפסקת התמיכה בעברית בצה"ל',
+'זאת ממש טרחה לבנות אתרים בשפה המציקה הזאת, אפשר לעבור לכל שפה שהיא בכיוון סטנדרטי. אם לא זה אז לפחות לשפר תמיכה בשפה ב Intellij', 3 );
 
 
 insert into company (id,name) values (  1,'Apple Inc.');
@@ -630,3 +632,4 @@ insert into computer (id,name,introduced,discontinued,company_id) values (574,'i
 
 delete from computer;
 delete from company;
+delete from project;
