45237271da135f453bb62feda5c853f1c8b8b733
Jenia Nemzer <jenian@gmail.com>
2013-10-16 16:19:21 +0300

    Moved SQL to a single file, it is clearer and faster for the demo.

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -12,7 +12,17 @@ create table project (
 
 create sequence project_seq start with 1000;
 
+
+insert into project (id,name, description, votes) values (  9,'Ninja turtles tribute site','What it sounds like', 0 );
+insert into project (id,name, description, votes) values (  10,'Coffee bags startup','Why should those tea bastards  enjoy progress alone ? ', 0 );
+insert into project (id,name, description, votes) values (  11,'Haskell support event','More people should know about Haskell', 2 );
+insert into project (id,name, description, votes) values (  12,'הפסקת התמיכה בעברית בצה"ל',
+'זאת ממש טרחה לבנות אתרים בשפה המציקה הזאת, אפשר לעבור לכל שפה שהיא בכיוון סטנדרטי. אם לא זה אז לפחות לשפר תמיכה בשפה ב Intellij', 3 );
+
+
 # --- !Downs
+
+delete from project;
 
 SET REFERENTIAL_INTEGRITY FALSE;
 
