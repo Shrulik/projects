dada9b0fb1c4e64cb0f6e446edc557663eb05cb2
Jenia Nemzer <jenian@gmail.com>
2013-10-16 16:21:05 +0300

    Moved everything I need to IntelliJ live templates, code remains in Application.java so I can test the relevant Angular code against it.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,17 +1,55 @@
 package controllers;
 
+import models.Project;
+import play.data.Form;
+import play.db.jpa.Transactional;
+import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
 
+import static play.data.Form.form;
 
-public class Application extends Controller   {
+
+public class Application extends Controller {
 
 
     public static Result index() {
-        return TODO;
+       return TODO;
     }
 
 
+    @Transactional(readOnly = true)
+    public static Result projects() {
+        return ok(Json.toJson(Project.findAll()));
+    }
+
+
+    @Transactional
+    public static Result addProject(){
+        Form<Project> boundForm = form(Project.class).bindFromRequest();
+
+        if ( boundForm.hasErrors())
+            return badRequest(boundForm.errorsAsJson());
+
+
+        Project project = boundForm.get();
+        project.save();
+
+        return ok(Json.toJson(project));
+
+    }
+
+    @Transactional()
+    public static Result updateProject(Long id){
+        Form<Project> boundForm = form(Project.class).bindFromRequest();
+
+        if ( boundForm.hasErrors() )
+            return badRequest(boundForm.errorsAsJson());
+
+        Project project = boundForm.get();
+        project.update(id);
+        return ok(Json.toJson(project));
+    }
 
 }
             
