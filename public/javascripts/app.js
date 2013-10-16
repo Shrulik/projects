d47c7a1f5986971ee3404a1c9115ba7fa56363f6
Yuri Kl <yurikl@gmail.com>
2013-10-16 23:27:05 +0300

    Added a section about what methods should be on controllers

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -3,14 +3,26 @@ var app = angular.module("Projects", []);
 app.controller('SimpleCtrl', ['$scope', 'Project', function ($scope, Project) {
 
     $scope.ourTitle = "Projects";
+    $scope.projects = Project.query();
+    $scope.newProject = {};
+
+    $scope.addProject = function () {
+        $scope.newProject.votes = 0;
+
+        Project.insert($scope.newProject, function (project) {
+            $scope.projects.then(function (projects) {
+                projects.push(project);
+            });
+            $scope.newProject = {};
+        },function (errors) {
+            $scope.messages = errors;
+        });
+
+    }
 
     $scope.toggleDesc = function (project) {
         project.show = !project.show;
-    }
-
-    $scope.projects = Project.query();
-
-
+    };
 }]);
 
 
@@ -21,6 +33,13 @@ app.service('Project', ['$http', function ($http) {
             return $http.get("/project").then(function (response) {
                 return response.data;
             });
+        },
+
+        insert: function (project, successCallback, errorCallback) {
+
+            $http.post('/project', project).
+                success(successCallback).error(errorCallback)
+
         }
     }
 }  ]);
