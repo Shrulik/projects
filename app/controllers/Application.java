d8b1c3e04515b7c0ddf92c53ac406cd2c1b45e76
Jenia Nemzer <jenian@gmail.com>
2013-10-16 14:05:34 +0300

    Comment belonged to sample project.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,23 +1,12 @@
 package controllers;
 
-import play.api.templates.Html;
-import play.libs.Json;
-import play.mvc.*;
-import play.data.*;
-import static play.data.Form.*;
-import play.db.jpa.*;
+import play.mvc.Controller;
+import play.mvc.Result;
 
-import models.*;
 
-/**
- * Manage a database of computers
- */
 public class Application extends Controller   {
 
 
-    /**
-     * Handle default path requests, redirect to computers list
-     */
     public static Result index() {
         return TODO;
     }
